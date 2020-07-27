a813de2c6e76b3eed244ecabc20b63d13ae506b3
Abhishek khanna <ABHISHEKKHANNA1995@GMAIL.COM>
2020-07-27 17:14:55 +0530

    Merge development into master (#1)
    * Add initial floor data
    
    * Add floor component
    
    * Add bootstrap css
    
    * Add business logic to application
    
    * feat: remove unwanted files
    
    * feat: add floor plan manager
    
    * Add service component
    
    * Remove extra files
    
    * Pass floor to actions directly
    
    * Assign to floorSetup for re-render
    
    * Floor setup
    
    Co-authored-by: Harshit <harshit.shrivastava@leadsquared.com>

--- app/components/floor.js
+++ app/components/floor.js
@@ -1,9 +1,15 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
-export default class PeopleListComponent extends Component {
-  @action
-  showPerson(person) {
-    alert(`The person's name is ${person}!`);
+export default class ApplicationComponent extends Component {
+  @service floorManager;
+
+  @action handleUp(fromFloor) {
+	this.floorManager.updateLiftPosition("up", fromFloor);
+  }
+
+  @action handleDown(fromFloor) {
+	this.floorManager.updateLiftPosition("down", fromFloor);
   }
 }
